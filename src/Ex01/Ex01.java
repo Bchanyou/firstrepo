c7bbd675caf2d99cd9df560635f8b9df6574b046
BCY <cksdb0907@naver.com>
2024-04-12 16:14:58 +0900

    두번째 커밋

--- src/Ex01/Ex01.java
+++ src/Ex01/Ex01.java
@@ -1,5 +1,12 @@
 package Ex01;
 
 public class Ex01 {
+	
+	public static void main(String[] args) {
+		System.out.println("개인 저장소 첫번째 커밋!");
+	}
+	private void syso() {
+		// TODO Auto-generated method stub
 
+	}"두번째 커밋"
 }
